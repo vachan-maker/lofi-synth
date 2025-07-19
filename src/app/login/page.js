0b1fdcec4f5e9880a32214ccb68b9b165ad7b504
vachan-maker <vachan@tutanota.com>
2025-07-19 22:41:53 +0530

    Push login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -44,7 +44,7 @@ export default function LogIn() {
     }
 
     try {
-      const response = await fetch(`${API_URL}/api/auth/login/`, {
+      const response = await fetch(`${API_URL}/api/auth/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
