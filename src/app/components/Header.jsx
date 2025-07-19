bdb36be8baa6e8908d4e2cec3853d59496312375
vachan-maker <vachan@tutanota.com>
2025-07-19 18:08:33 +0530

    Login page

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,9 +1,9 @@
-
+import Link from "next/link"
 export default function Header() {
     return (
         <header className="w-full py-6 px-12 flex justify-between">
         <p className="text-2xl font-bold bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">Lofi Synth</p>
-        <button className=" bg-gradient-to-r from-purple-500 to-pink-500 text-white py-2 px-6 rounded-full">Login</button>
+        <Link href="/login"><button className=" bg-gradient-to-r from-purple-500 to-pink-500 text-white py-2 px-6 rounded-full cursor-pointer">Login</button></Link>
         </header>
     )
 }
