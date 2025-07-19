e0fd094b42ab0063417702d5ee1e9e7c974e1b2f
vachan-maker <vachan@tutanota.com>
2025-07-19 22:11:19 +0530

    Form upload for image upload

--- src/app/components/ImageUpload.jsx
+++ src/app/components/ImageUpload.jsx
@@ -7,6 +7,7 @@ export default function ImageUpload() {
                 <div className="flex flex-col items-center justify-center">
                     <Cloud color="#ed3ba8" size={90} className="mb-15"/>
                     <div>
+                        <form action="/upload" method="POST">
                 <label
                     for="file-upload"
                     className="inline-block cursor-pointer bg-gradient-to-r from-purple-500 to-pink-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:scale-95"
@@ -16,9 +17,10 @@ export default function ImageUpload() {
                 <input
                     id="file-upload"
                     type="file"
-                    class="hidden"
+                    className="hidden"
                 />
-                <button className="cursor-pointer bg-gradient-to-r from-blue-400 to-pink-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:scale-95 ml-5">Let's Go!</button>
+                <button type="submit" className="cursor-pointer bg-gradient-to-r from-blue-400 to-pink-500 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:scale-95 ml-5">Let's Go!</button>
+                </form>
                 </div>
                 </div>
             </div>
